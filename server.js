671f5cf8d7143bbdf192a3eb59dadf16f9eb4ff5
Eddie Yao <adpoyao@gmail.com>
2017-10-10 13:12:34 -0700

    added gateKeeper middleware

--- server.js
+++ server.js
@@ -4,23 +4,6 @@ const express = require('express');
 const app = express();
 const queryString = require('query-string');
 
-
-//The app you'll be writing this middleware for has a single endpoint, /api/users/me
-
-
-
-// For this challenge, we're hard coding a list of users, because
-// we haven't learned about databases yet. Normally, you'd store
-// user data in a database, and query the database to find
-// a particular user.
-//
-// ALSO, for this challenge, we're storing user passwords as
-// plain text. This is something you should NEVER EVER EVER 
-// do in a real app. Instead, always use cryptographic
-// password hashing best practices (aka, the tried and true
-// ways to keep user passwords as secure as possible).
-// You can learn more about password hashing later
-// here: https://crackstation.net/hashing-security.htm
 const USERS = [
   {id: 1,
     firstName: 'Joe',
@@ -28,7 +11,6 @@ const USERS = [
     userName: 'joeschmoe@business.com',
     position: 'Sr. Engineer',
     isAdmin: true,
-    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
     password: 'password'
   },
   {id: 2,
@@ -37,7 +19,6 @@ const USERS = [
     userName: 'sallystudent@business.com',
     position: 'Jr. Engineer',
     isAdmin: true,
-    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
     password: 'password'
   },
   {id: 3,
@@ -46,7 +27,6 @@ const USERS = [
     userName: 'lila@business.com',
     position: 'Growth Hacker',
     isAdmin: false,
-    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
     password: 'password'
   },
   {id: 4,
@@ -55,51 +35,33 @@ const USERS = [
     userName: 'freddy@business.com',
     position: 'Community Manager',
     isAdmin: false,
-    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
     password: 'password'
   }
 ];
 
-// app.get('/', (req, res) => {
-//   res.send('hello');
-// });
-
 // write a `gateKeeper` middleware function that:
-//  1. looks for a 'x-username-and-password' request header
-//  2. parses values sent for `user` and `pass` from 'x-username-and-password'
-//  3. looks for a user object matching the sent username and password values
-//  4. if matching user found, add the user object to the request object
-//     (aka, `req.user = matchedUser`)
 function gateKeeper(req, res, next) {
+  //  1. looks for a 'x-username-and-password' request header
   const headerUserPass = req.get('x-username-and-password');
   console.log(headerUserPass);
-  console.log(queryString.parse(headerUserPass));
-  //const {user, password} = req.query;
-  // console.log('user', user, 'password', password);
-  // res.send(`user', ${user}, 'password', ${password}`);
-  // res.json({user, password});
-  // queryString.parse(req.user)
-  // const headerUserPass = req.get(users)
+  //  2. parses values sent for `user` and `pass` from 'x-username-and-password'
+  const parsedUserPass = (queryString.parse(headerUserPass));
+  console.log(parsedUserPass);
+  //  3. looks for a user object matching the sent username and password values
+  //  4. if matching user found, add the user object to the request object
+  //     (aka, `req.user = matchedUser`)
+  req.user = USERS.find(user => {
+    return user['userName'] === parsedUserPass.user;
+  });
   next();
 }
 
-app.get('/', gateKeeper);
 
-
-
-// Add the middleware to your app!
-
-// this endpoint returns a json object representing the user making the request,
-// IF they supply valid user credentials. This endpoint assumes that `gateKeeper` 
-// adds the user object to the request if valid credentials were supplied.
-app.get('/api/users/me', (req, res) => {
+app.get('/api/users/me', gateKeeper, (req, res) => {
   // send an error message if no or wrong credentials sent
   if (req.user === undefined) {
     return res.status(403).json({message: 'Must supply valid user credentials'});
   }
-  // we're only returning a subset of the properties
-  // from the user object. Notably, we're *not*
-  // sending `password` or `isAdmin`.
   const {firstName, lastName, id, userName, position} = req.user;
   return res.json({firstName, lastName, id, userName, position});
 });
